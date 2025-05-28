de16005eab0750e1d256b8b7aac043b973cdaf58
Gothax <scott990328@gmail.com>
2025-05-29 03:09:12 +0900

    test: add simple test

---
+++ src/test/java/com/example/jenkinstest/SimpleTest.java
@@ -0,0 +1,26 @@
+package com.example.jenkinstest;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class SimpleTest {
+
+    @Test
+    @DisplayName("1+1 test")
+    void onePlusOneTest() {
+        // given
+        int a = 1;
+        int b = 1;
+        // when
+        int result = a + b;
+        // then
+        assertThat(result).isEqualTo(2);
+
+    }
+
+
+}
